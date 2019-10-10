af4ed904690d406bd5c6da6bde7bcf804cbb17b2
Wang Shiyu <syyx20046@126.com>
2019-10-10 14:30:55 +0800

    fix branch

--- LoveEggplant/src/main.cpp
+++ LoveEggplant/src/main.cpp
@@ -20,6 +20,7 @@ brain Brain;
 controller Controller = controller();
 //DongYunZhang //4 11 10 20 1 3 9 8 D A C 
 //PeiLianChe   //12 13 15 14 8 19 9 5 F E C
+//Wangshengfei  //3 15 7 6 4 10 11 13 A E C
 
 motor LeftMotor1  (PORT3,   gearSetting::ratio18_1, false);
 motor LeftMotor2  (PORT15,   gearSetting::ratio18_1, false);
